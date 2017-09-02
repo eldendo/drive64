ec9be945f0798cd326b134079a0879125f9fd986
eldendo <marc.dendooven@gmail.com>
2017-09-02 12:50:46 +0200

    Add files via upload

--- drive64.pas
+++ drive64.pas
@@ -11,21 +11,15 @@ var drive: file of byte;
     SpT: array[1..40] of integer;
     buf: buffer;
     t: track;
+    j: integer;
 
-    procedure rdBuf(t: track; s: sector);
+    procedure rdBuf(t: track; s: sector; var buf: buffer);
     var i: track;
 	sn : integer = 0;
-	j: byte;
     begin
 	for i := 2 to t do sn := sn + SpT[i-1];
-//	rdBuf := sn + s;
-	seek(drive, sn+s-1);
-	blockRead(drive,buf,1);
-	for j := 0 to 255 do 
-	    begin 
-		if j mod 8 = 0 then begin writeln; write(hexstr(j,4),': ') end; 
-		write(hexStr(buf[j],2),' ')
-	    end
+	seek(drive, sn+s);
+	blockRead(drive,buf,1)
     end;
 
 
@@ -33,16 +27,28 @@ begin
 
     writeln('welcome to drive64');
     writeln('(c)2017 by ir. Marc Dendooven');
+    writeln('-----------------------------');
     
     for t := 1  to 17 do SpT[t]:=21;
     for t := 18 to 24 do SpT[t]:=19;
     for t := 25 to 30 do SpT[t]:=18;
     for t := 31 to 40 do SpT[t]:=17;
     
-    assign(drive,'Fort Apocalypse.d64');
+    assign(drive,'files/1988-11.d64');
     reset(drive, 256);
     
-    rdBuf(18,1);
+    rdBuf(18,0,buf);
+    writeln('DOS version: ', chr(buf[$02]));
+    write('Disk Name: '); for j := $90 to $9F do if buf[j] <> $A0 then write(chr(buf[j])); writeln;
+    writeln('DOS type: ', chr(buf[$A5]), chr(buf[$A6]));
+
+    
+    for j := 0 to 255 do 
+	begin 
+	    if j mod 8 = 0 then begin writeln; write(hexstr(j,4),': ') end; 
+	    write(hexStr(buf[j],2),' ')
+	end;
+
     
     close(drive)
 end.
