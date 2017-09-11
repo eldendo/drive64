24f741d840560dc0b8d8d478fb901f6076ed92be
eldendo <marc.dendooven@gmail.com>
2017-09-11 22:05:20 +0200

    Add files via upload

--- drive64.pas
+++ drive64.pas
@@ -10,8 +10,9 @@ type track = 1..40;
 var drive: file of byte; 
     SpT: array[1..40] of integer;
     buf: buffer;
-    t: track;
-    j: integer;
+    t,dt: integer; //track;
+    i,j: integer;
+    ds: sector;
 
     procedure rdBuf(t: track; s: sector; var buf: buffer);
     var i: track;
@@ -41,14 +42,27 @@ begin
     writeln('DOS version: ', chr(buf[$02]));
     write('Disk Name: '); for j := $90 to $9F do if buf[j] <> $A0 then write(chr(buf[j])); writeln;
     writeln('DOS type: ', chr(buf[$A5]), chr(buf[$A6]));
-
+    writeln('-----------------------------');
+    dt := 18; ds := 1;
     
-    for j := 0 to 255 do 
-	begin 
-	    if j mod 8 = 0 then begin writeln; write(hexstr(j,4),': ') end; 
-	    write(hexStr(buf[j],2),' ')
-	end;
+    while dt <> 0 do 
+	begin
+	    rdBuf(dt,ds,buf);
+	    dt := buf[0]; ds := buf[1]; 
+	    
+	    for j := 0 to 7 do
+		begin
+		    for i := j*32+$5 to j*32+$14 do if buf[i] <> $A0 then write(chr(buf[i])); writeln;
+		end;
+ 
 
-    
+(*
+	    for j := 0 to 255 do 
+		begin 
+		    if j mod 8 = 0 then begin writeln; write(hexstr(j,4),': ') end; 
+		    write(hexStr(buf[j],2),' ')
+		end
+*)		
+    	end;
     close(drive)
 end.
